54409ff7c57d8b1d3142da16b82ffd471c26ea05
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-21 17:52:48 +0300

    Changing App.js and css

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
-}
+    );
+  }
+} 
 
 export default App;
