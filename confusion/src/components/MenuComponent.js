5a7c3a3d75059774cd36a3a0eff1070768d00f3d
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-24 08:40:53 +0300

    React Components

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
+
 class Menu extends Component {
 
     constructor(props) {
@@ -8,57 +10,32 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
-
-        console.log('Menu Component constructor is invoked');
     }
 
-    onDishSelect(dish){
+    onDishSelect(dish) {
         this.setState({ selectedDish: dish });
     }
 
-    renderDish(dish){
-        if(dish != null){
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }else{
-            return (
-                <div></div>
-            );
-        }
-    }
-    componentDidMount(){
-        console.log('Menu Component componentDidMount is invoked');
-    }
     render() {
-        
-        const menu = this.props.dishes.map((dish) => {
+        const menu = this.props.dishes.map(dish => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 mt-1">
+                <div key={dish.id} className="col-12 col-md-5 m-1">
                     <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay body className="ml-5">
+                        <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
                         </CardImgOverlay>
                     </Card>
                 </div>
-            );
+            )
         });
-        console.log('Render Component render is invoked');
+
         return (
             <div className="container">
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
+                <DishDetail dish={this.state.selectedDish} />
             </div>
         );
     }
