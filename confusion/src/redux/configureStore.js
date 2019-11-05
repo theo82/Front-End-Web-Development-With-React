9df9ba22fadddc382d203e53ae522f8dbaca32f8
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-11-05 12:16:07 +0200

    Combining Reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -2,13 +2,20 @@
  * Here we crate a store. It has a Reducer and a state. 
  */
 
-import { createStore } from 'redux';
-import { Reducer, initialState} from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, 
-        initialState
+       combineReducers({
+           dishes: Dishes,
+           comments: Comments,
+           promotions: Promotions,
+           leaders: Leaders
+       })
     );
 
     return store;
